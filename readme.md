19fdc3e9654c69479fe8c125715b1c3f117734cd
-e <iandavidbanks@gmail.com>
2022-02-01 00:34:26 -0600

    Added another visual test and updated the readme

--- readme.md
+++ readme.md
@@ -1,9 +1,7 @@
 # Plugin: Kirby 3.6 Vue Props
 This plugin serves as a proof of concept to demonstrate irregularities with how Kriby handles passing and handling props between a plugins `index.php` file and how it receives those props in a custom vue component.
 
-## How to use this plugin:
-
-### Setup
+## Setup
 * Create a new Kirby 3.6.x instance `composer create-project getkirby/starterkit test-site`
 * Navigate to `/site`
 * Create a plugins dir (if one does not exist)
@@ -14,10 +12,12 @@ This plugin serves as a proof of concept to demonstrate irregularities with how 
 
 You should now see a simple 2 column layout with 2 images. 
 
-### Testing
-#### Image Test
+## Testing
+### Image Test
 * Observer the right and left images.
 * The image on the left uses the vue prop `src`. It initially loads in the image, but after roughly 10 seconds, the image disappears.
 * The image on the right uses the vue data object `src2`. It loads and will remain after the first image disappears.
 
-#### JSON Object Test
+### JSON Object Test
+* Observe the "On Load" top section. This displays the values that are in the `_props` object when the page is loaded / when the component is created.
+* Observe the "On Fetch/XHR" bottom section. This displays the values that are in the `_props` object after the fetch event that clears out the props. 
--- src/components/sections/imagerandomizer.vue
+++ src/components/sections/imagerandomizer.vue
@@ -1,20 +1,41 @@
 <template>
-  <div class="fqsr">
-    <h2 class="k-headline">Images</h2>
-    <div class="images_wrapper">
+  <div class="">
+    <section>
+      <header>
+        <h1 class="test-headline">Image Test</h1>
+      </header>
+      <div class="wrapper">
+        <div class="image_wrapper">
+          <h2 class="item-header">Prop Image</h2>
+          <p>This image gets it's src attribute from a vue prop.</p>
+          <img :src="src" width="300" height="300"
+               alt="Oh no what happened?! Well, it appears that when using props in a Vue component, it somehow makes it no longer available. When the backend page refresh happens (roughly every 10 seconds) the prop no longer exists and the reactive change occurs, the data is undefined. See JSON Test below.">
+        </div>
 
-      <div class="image_wrapper">
-        <h2 class="item-header">Prop Image</h2>
-        <img :src="src" width="300" height="300"
-             alt="Oh no what happened?! Well, it appears that when using props in a Vue component, it somehow makes it no longer available. When the backend page refresh happens (roughly every 10 seconds) the prop no longer exists and the reactive change occurs, the data is undefined." >
+        <div class="image_wrapper">
+          <h2 class="item-header">Non-Prop Image</h2>
+          <p>This image gets it's src attribute from a vue data var.</p>
+          <img :src="src2" width="300" height="300"/>
+        </div>
+
       </div>
+    </section>
+    <section>
+      <header>
+        <h1 class="test-headline">JSON Test</h1>
+      </header>
+      <div class="wrapper columns">
+        <div class="column">
+          <h2 class="item-header">On Load </h2>
+          <pre><strong>this._props:</strong>{{ props1 }}</pre>
+        </div>
+        <div class="column">
+          <h2 class="item-header">On fetch/xhr</h2>
+          <pre><strong>this._props:</strong>{{ props2 }}</pre>
+        </div>
 
-      <div class="image_wrapper">
-        <h2 class="item-header">Non-Prop Image</h2>
-        <img :src="src2" width="300" height="300"/>
       </div>
-
-    </div>
+    </section>
   </div>
 </template>
 <script>
@@ -24,8 +45,12 @@ export default {
     src: String
   },
   data() {
+    /*this.props1 = "";
+    this.props2 = "";*/
     return {
-      src2: ""
+      src2: "",
+      props1: {},
+      props2:{}
     }
   },
   created() {
@@ -35,31 +60,72 @@ export default {
       this.src = response.src;
       this.src2 = response.src2;
       console.log(this);
+      this.props1 = JSON.stringify(this._props);
+      this.checkPropsInTen();
     });
   },
   updated() {
     console.log('updated');
     console.log(`src: ${this._props.src}`);
+  },
+  methods: {
+    checkPropsInTen: function(){
+      setTimeout(()=>{
+        this.props2 = JSON.stringify(this._props);
+      }, 10500);
+    }
   }
 }
 </script>
 <style lang="scss" scoped>
-.images_wrapper {
+
+section {
+  border: 1px solid #000;
+  border-radius: 5px;
+  padding: 10px;
+
+  & + section {
+    margin-top: 10px;
+  }
+}
+
+.test-headline {
+
+}
+
+.wrapper {
   display: flex;
 
   .image_wrapper {
     width: 300px;
     margin-right: 10px;
 
-    img{
+    img {
       width: 300px;
       height: 300px;
     }
   }
+
+  &.columns{
+    flex-direction: column;
+  }
+  .column{
+    margin-top: 10px;
+
+    .item-header{
+      display:inline-block;
+    }
+  }
 }
 
-h2.item-header{
+h2.item-header {
+  margin-top:20px;
+  margin-bottom: 10px;
   font-weight: bold;
   border-bottom: 1px solid black;
+  font-size: 1.2rem;
+}
+p{
+  margin-bottom:10px;
 }
 </style>
